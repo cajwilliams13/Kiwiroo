81fc028ff2a4861600331d4f8e4fdf512962ab48
23019883 <Cajwilliams13@gmail.com>
2019-07-07 11:35:12 +1000

    kek

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const botconfig = require('./config.json')
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./Commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./Commands/${file}`);
